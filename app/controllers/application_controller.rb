5527c082c12879fce6a9c2c9398bf191d578b1ff
Ashley Mattam <amattam427@gmail.com>
2022-02-16 02:14:35 -0500

    http requests-post, patch, get

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,48 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/accommodations" do
+    accommodations = Accommodation.all
+    accommodations.to_json
   end
+
+  get "/accommodations/:id" do
+    accommodation = Accommodation.find(params[:id])
+    accommodation.to_json(include: :reviews)
+  end
+
+  get "/accommodations/likes/:id" do
+    accommodation = Accommodation.find(params[:id])
+    accommodation.to_json(only: :like)
+  end
+
+  patch "/accommodations/:id" do 
+    accommodation = Accommodation.find(params[:id])
+    accommodation.update(
+      like: params[:like]
+    )
+    review.to_json
+  end
+
+  get "/reviews" do 
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  get "/reviews/:id" do 
+    reviews = Review.find(params[:id])
+    reviews.to_json
+  end
+
+  post "/reviews" do 
+    review = Review.create(
+      comment: params[:comment],
+      accommodation_id: params[:accommodation_id]
+    )
+    review.to_json
+  end
+  
+
+  
 
 end
